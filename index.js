d559feb12d130011c10d4b36b23eb1c4cc1b94c0
wangfei <wangfei.shine@gmail.com>
2017-10-14 15:12:52 +0800

    onepage application

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+var root = document.createElement('div');
+root.innerHTML = '<p><a href="html-category.html">HTML元素分类</a></p><p><a href="html-attribute.html">HTML元素属性</a></p><p><a href="html-text.html">HTML文本</a></p><p><a href="html-img.html">HTML图片</a></p><p><a href="html-css.html">CSS</a></p><p><a href="html-jsbasic.html">JS基础S</a></p>';
+
+document.querySelector("body").appendChild(root);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
 	entry: {
+        './index': './index.js',
         './hello': './hello.js',
         './calc': './calc.js'
     },
